6b9dbb56efc87a779c23036a79a68743e160e20f
ListenToAJ <simaospamm@gmail.com>
2025-02-05 17:45:42 -0500

    UI mockup

---
+++ script.js
@@ -0,0 +1,12 @@
+$(document).ready( function() {
+    // $(".card").tap(function() {
+    //     alert("HI KAT!!!!!!!");
+    // });
+
+    $("#playlist_title").on("click touchstart", function (e) { 
+        alert("Reloading...");
+        location.reload();
+     });
+
+     $("")
+});
