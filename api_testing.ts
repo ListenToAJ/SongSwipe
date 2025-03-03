200a36772fd6d74d7cef74eec0ac0f1ec9377365
cjcocokrisp <chriscoco1205@gmail.com>
2025-03-03 14:34:59 -0500

    add spotify preview test

--- api_testing.ts
+++ api_testing.ts
@@ -1,5 +1,6 @@
 import { SpotifyApi } from '@spotify/web-api-ts-sdk';
 import dotenv from 'dotenv';
+import { getSpotifyPreviewUrl } from './spotify-preview.ts';
 
 dotenv.config();
 
@@ -9,6 +10,10 @@ dotenv.config();
     const items = await sdk.search("kiseki (peak goat raw fire)", ["playlist"]);
     const my_playlist = items.playlists.items[0];
 
-    const tracks = await sdk.playlists.getPlaylistItems(my_playlist.id)
+    const tracks = await sdk.playlists.getPlaylistItems(my_playlist.id);
+    console.log(tracks.items.length);
     console.log(tracks.items.map((item) => item.track.name));
+
+    const preview_url = await getSpotifyPreviewUrl(tracks.items[0].track.id);
+    console.log(preview_url);
 })();
