08796dc5e7711b00fe5df6de1eb0c8d1cc6a949b
cjcocokrisp <chriscoco1205@gmail.com>
2025-04-15 20:29:29 -0400

    feat: add endpoints for usage metrics for user testing

--- src/api.ts
+++ src/api.ts
@@ -1,7 +1,7 @@
 import express, { Router } from 'express'
 import serverless from 'serverless-http'
 import cors from 'cors';
-import { authCallback, authLogin, authRefresh, playlistBuild, playlistData, userData, userPlaylists, songPreview, songRemove, playlistCreate, songAdd } from './endpoints';
+import { authCallback, authLogin, authRefresh, playlistBuild, playlistData, userData, userPlaylists, songPreview, songRemove, playlistCreate, songAdd, metricsDecision, metricsElapsed, metricsInformation } from './endpoints';
 
 const app = express();
 const router = Router();
@@ -25,6 +25,10 @@ router.post('/playlist/add', songAdd);
 router.delete('/playlist/remove', songRemove);
 // Song related endpoints
 router.get('/song', songPreview);
+// Testing Metrics endpoints
+router.post('/metrics/information', metricsInformation);
+router.post('/metrics/decision', metricsDecision);
+router.post('/metrics/elapsed', metricsElapsed);
 
 // Netlify API setup
 app.use("/.netlify/functions/api", router);
